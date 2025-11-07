1ee2676506304d4c4770d923529b30b31140c0f5
kishore-1812 <pkichu1997@gmail.com>
2025-11-07 15:50:38 +0100

    adjusting logic inside target application

--- src/fastapi/main.py
+++ src/fastapi/main.py
@@ -21,3 +21,34 @@ def login(
     password: str = Form(...)
 ):
     return {"message": f"Welcome {user}"}
+
+
+# from fastapi import FastAPI, Form
+# import logging
+# import os
+
+# app = FastAPI()
+
+# API_SECRET = "SuperSecretPassword123!"  # <-- Hardcoded credential
+
+# logging.basicConfig(
+#     filename='/app/logs/fastapi.log',
+#     level=logging.INFO,
+#     format='%(asctime)s - %(levelname)s - %(message)s'
+# )
+
+# @app.get("/health")
+# def health():
+#     return {"status": "alive"}
+
+# @app.post("/login")
+# def login(user: str = Form(...), password: str = Form(...)):
+#     return {"message": f"Welcome {user}"}
+
+# @app.get("/runcmd")
+# def run_cmd():
+#     os.system("ls -l /")  # <-- External command execution
+
+# @app.get("/echo")
+# def echo(q: str):
+#     return {"result": q}  # <-- Reflected user input
